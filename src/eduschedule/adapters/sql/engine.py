004358a9896bece50e2f4b86bf366a075bce5413
ColinChester <colin.chester@bison.howard.edu>
2025-09-03 11:22:54 -0400

    Refactored engine to read URL at call time

--- src/eduschedule/adapters/sql/engine.py
+++ src/eduschedule/adapters/sql/engine.py
@@ -1,18 +1,33 @@
+from __future__ import annotations
+import os
 from contextlib import contextmanager
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
-from eduschedule.config import DATABASE_URL
 
-ENGINE = create_engine(DATABASE_URL, future=True, pool_pre_ping=True)
-localSession = sessionmaker(bind=ENGINE, autoflush=False, autocommit=False, future=True)
+def _dbUrl() -> str:
+    return os.getenv("DATABASE_URL", "sqlite:///./eduschedule.db")
+
+def _makeEngine():
+    eng = create_engine(_dbUrl(), future=True)
+    @event.listens_for(eng, "connect")
+    def foreignKeyOn(dbapiConn, _):
+        try:
+            dbapiConn.execute("PRAGMA foreign_keys=ON")
+        except Exception:
+            pass
+    return eng
 
 @contextmanager
 def session_scope():
-    s = localSession()
+    eng = _makeEngine()
+    Session = sessionmaker(bind=eng, autoflush=False, autocommit=False, future=True)
+    s = Session()
     try:
         yield s
         s.commit()
-    except:
+    except Exception:
+        s.rollback()
         raise
     finally:
         s.close()
+        eng.dispose()
